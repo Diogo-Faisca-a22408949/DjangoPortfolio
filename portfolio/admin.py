22bc5e8db1b0e931a7bad76630e9da27f126ca44
Faísca <134933583+Diogo-Faisca-a22408949@users.noreply.github.com>
2026-04-11 12:20:00 +0000

    Configuração do Django Admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(TFC)
+class TFCAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'curso', 'ano')
+    search_fields = ('titulo', 'autores')
+    list_filter = ('curso', 'ano')
+
+admin.site.register(Licenciatura)
+admin.site.register(Tecnologia)
+admin.site.register(UnidadeCurricular)
+admin.site.register(Projeto)
+admin.site.register(MakingOf)
